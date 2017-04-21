4e3a726b4af38df8f069a2ef4ca692f6e25e5933
yuki <nkc.yuki.koyama@gmail.com>
2017-04-21 12:36:40 +0900

    GUIの表示・非表示を切り替えるスクリプト作成中

---
+++ Poyon/Assets/Escortselector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Escortselector : MonoBehaviour {
+
+    
+
+    // Use this for initialization
+    void Start ()
+    {
+        gameObject.SetActive(false);		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            gameObject.SetActive(true);
+        }
+	}
+}
