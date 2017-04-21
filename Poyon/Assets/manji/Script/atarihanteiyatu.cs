6b813bd12043f8e5622ba290574e74bcf8d327f5
keitatakahasi <nkc.keita.takahashi@gmail.com>
2017-04-21 12:14:53 +0900

    tag Wallに当たると値を返す奴

---
+++ Poyon/Assets/manji/Script/atarihanteiyatu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class atarihanteiyatu : MonoBehaviour
+{
+    public int status;
+    public Text statusUI;
+    private void Start()
+    {
+        status = 0;
+    }
+    private void Update()
+    {
+        if(status == 0)
+        {
+            statusUI.text = "白";
+        }
+        else
+        {
+            statusUI.text = "黒";
+        }
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        string yourTag = collision.gameObject.tag;
+
+        if (yourTag == "Wall")
+        {
+            status =1;
+        }
+    }
+}
